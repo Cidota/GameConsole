3b6cd20aa0ab92bd0ae5adb391e07ebd465843e6
#60705208 <60705208+Seyte@users.noreply.github.com>
2022-05-25 13:26:04 +0200

    WIP : Adding comments and doxygen

--- Game/Inc/gameEngine.h
+++ Game/Inc/gameEngine.h
@@ -1,5 +1,13 @@
+/**
+ * @file gameEngine.h
+ * @brief Functions to run all games.
+ * @details See @ref index for further details.
+ * @copyright Cidota. All rights reserved, 2022.
+ **/
+
 #ifndef __GAME_ENGINE_H__
 #define __GAME_ENGINE_H__
+
 #include "snake.h"
 #include "input.h"
 #include "log.h"
@@ -17,43 +25,84 @@
 
 #define offset 160
 
+// renderOthers is true when the UI and background of a game must be render.
+static bool renderOthers = true;
+
+/**
+ * @brief Different games available.
+ **/
 typedef enum games
 {
-    NONE,
-    SNAKE_GAME
+    NONE, /**< No game */
+    SNAKE_GAME /**< Snake game */
 } games;
 
+/**
+ * @brief Structure for positions
+ */
 struct coordinate
 {
     int x;
     int y;
 };
 
+/**
+ * @brief Structure of a sprite
+ */
 struct sprite
 {
-    struct coordinate position;
-    struct coordinate size;
-    enum color sprite;
+    struct coordinate position; /**< Top left corner of the sprite. */
+    struct coordinate size;     /**< Size of the sprite. */
+    enum color sprite;          /**< Color of the sprite. */
 };
 
+/**
+ * @brief Structure of the things to render in the following frame.
+ */
 struct screen_updates
 {
-    unsigned int size;
-    struct sprite *sprites;
+    unsigned int size;         /** Max size available for the sprites array*/
+    unsigned int currentIndex; /** Current position of the sprite array*/
+    struct sprite *sprites;    /** Sprite array */
 };
+
 void setRenderNonGameElementsTrue();
+/**
+ * @brief Initialize the game engine.
+ * @details Must be called only once.
+ **/
 void init();
 
+/**
+ * @brief Initialize a choosen game.
+ * @param game game to initialize from the enum games.
+ **/
 void init_game(games game);
 
+/**
+ * @brief Update the game currently running.
+ **/
 void update();
 
+/**
+ * @brief Add a sprite/color to render next frame.
+ * @param posX left corner of the sprite.
+ * @param posY top corner of the sprite
+ * @param sizeX width of the sprite.
+ * @param sizeY height of the sprite.
+ * @param sprite color/image of the sprite.
+ **/
 void addSpriteUpdate(int posX, int posY, int sizeX, int sizeY, color sprite);
 
+/**
+ * @brief Reset the list of updates to render
+ * @details Must be called after having render all the sprites.
+ **/
 void resetSpriteUpdate();
 
+/**
+ * @brief Render all the changes of a frame.
+ **/
 void render();
-
-// void end_game();
 
 #endif
